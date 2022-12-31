ced4cc3a32b6e2631a3e3ae69ef82cd19e1bed1d
lwmeek <timicivey27@gmail.com>
2022-12-31 13:15:04 -0500

    finishing dark theme appearance

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const toggle = document.querySelector(".toggle");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const container = document.querySelector(".main_container");
 
 toggle.addEventListener("click", () => {
     event.preventDefault();
@@ -7,10 +8,12 @@ toggle.addEventListener("click", () => {
     if(!toggle.classList.contains("backward")) {
         toggle.classList?.add("backward");
         toggle.classList?.remove("forward");
-        body.classList?.add("black_theme");
+        body.classList?.add("dark_theme");
+        container.classList?.add("dark_theme");
     } else {
         toggle.classList?.remove("backward");
         toggle.classList?.add("forward");
-        body.classList?.remove("black_theme");
+        body.classList?.remove("dark_theme");
+        container.classList?.remove("dark_theme");
     }
 })
